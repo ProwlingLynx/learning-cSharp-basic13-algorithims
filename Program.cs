37d67d42c27e710868510050ce1c16aabad9acc4
Javier Tamez <tamezjavier@gmail.com>
2022-12-13 15:10:01 -0600

    Implement print values up to 255 methods.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using myalgo.algoclasses;
+
+
+
+namespace myalgo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                Printer255 testPrint255 = new Printer255();
+                testPrint255.print255Nums();
+                testPrint255.printOddNums255();
+                testPrint255.print255PlusSum();
+                int[] testArray = createArrayOfIntsOfNSized(10);
+                Console.WriteLine("Expect nums 0 - 9 values in size 10 array------------");
+                Console.WriteLine(String.Join(", ", testArray));
+                Console.WriteLine("Array test end----------");
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Test failed!");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public static int[] createArrayOfIntsOfNSized(int n)
+        {
+            int[] ints = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                ints[i] = i;
+            }
+            return ints;
+        }
+    }
+}
---
+++ algoclasses/Printer255.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myalgo.algoclasses
+{
+    internal class Printer255
+    {
+        public void print255Nums()
+        {
+            Console.WriteLine("Printing nums 1 - 255-------------");
+            for (int i = 1; i <= 255; i++)
+			    {
+                    Console.WriteLine(i);
+			    }
+            Console.WriteLine("Print finish------------------");
+        }
+        public void printOddNums255()
+        {
+            Console.WriteLine("Printing odd nums up to 255------------");
+            for (int i = 1; i <= 255; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Odd Print finished---------------");
+        }
+        public void print255PlusSum()
+        {
+            Console.WriteLine("Printing sum plus nums [0 - 255]------------");
+            int totalSoFar = 0;
+            for (int i = 0; i <= 255; i++)
+            {
+                totalSoFar += i;
+                Console.WriteLine($"The current number is: {i}. Total so far is: {totalSoFar}");
+            }
+            Console.WriteLine("Printing End---------------");
+
+        }
+    }
+}
