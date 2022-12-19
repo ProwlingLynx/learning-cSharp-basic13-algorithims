dee111fb39ad030ed8cc56bcf7d15fed2f963f5b
Javier Tamez <tamezjavier@gmail.com>
2022-12-19 16:33:29 -0600

    Create iterations for array.

--- Program.cs
+++ Program.cs
@@ -23,10 +23,45 @@
             int total = 0; // Our running total of i.
             for (int i = 0; i < 256; i++)
             {
-                Console.WriteLine("Current number is: " + i);
+                Console.WriteLine("Current number is: " + i); // Strings are immutable. This operation is not modifying the old string. It makes a new String.
                 total = total + i; // We could write this as total += i
                 Console.WriteLine("Total so far: " + total);
             }
+
+            // Print the values in an array and intializing an array literal
+            int[] intArray = { 1, 3, 5, 7, 9, 13 }; // Could also use new int[] { 1, 2, 3, 4...}
+            for (int index = 0; index < intArray.Length; index++) // intArray.Length accesses the Length property of the array. Useful for iterating unknown sized arrays
+            {
+                Console.WriteLine(intArray[index]); // Syntax note: index is an integer value. putting it in the brackets means we are accessing the value at the specified index
+            }
+
+            // Print the largest value in the array.
+            int maxSoFar;
+            maxSoFar = intArray[0]; // We will use the first value of the previous array as the starting value of maxSoFar
+            for (int index = 1; index < intArray.Length; index++) // We start at 1 b/c we are using the value at index 0
+            {
+                int value = intArray[index];
+                if (value > maxSoFar)
+                {
+                    maxSoFar = value;
+                }
+            }
+            Console.WriteLine("The max value is: " + maxSoFar);
+
+            // Should work with negative values too
+            int[] negativeArray = { -1, -30, 40, 10, -5, 50, -3 };
+            maxSoFar = negativeArray[0]; // overriding the value of maxSoFar.
+            for (int index = 1; index < negativeArray.Length; index++) // Note: index is scoped to the for loop so we are technically creating a new variable named index each time.
+            {
+                int value = negativeArray[index];
+                if (value > maxSoFar)
+                {
+                    maxSoFar = value;
+                }
+            }
+            Console.WriteLine("The max value of our mixed array is: " + maxSoFar);
+
+            // Find
         }
     }
 }
